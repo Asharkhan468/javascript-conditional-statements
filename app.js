15765a235ef18a224530b026195e44ba61f17954
Asharkhan468 <asharullah.khan2468@gmail.com>
2024-02-28 14:16:13 +0500

    Javascript Conditional statement

---
+++ app.js
@@ -0,0 +1,50 @@
+// Qusetion no 1
+
+// let n=+prompt("Enter a Number");
+
+// if(n%3==0){
+//     console.log(n + " is divisible by 3");
+// }
+
+// else{
+//     console.log(n + " is not divisible by 3");
+// }
+
+
+// Qusetion no 2
+
+// let a=+prompt("Enter a Number");
+
+// if (a%2==0){
+//     console.log(a + " is a Even Number")
+// }
+// else {
+//     console.log(a + " is a Odd Number");
+// }
+
+
+// Qusetion no 3
+
+// let b=+prompt("Enter your Age");
+
+// if (b>18){
+//     console.log("Old enough");
+// }
+
+// else {
+//     console.log("Too Young");
+// }
+
+
+
+// Qusetion no 4
+
+let c=prompt("Enter Your Name");
+
+if(c="Ashar"){
+    console.log("Hello! Ashar");
+}
+
+else{
+    
+}
